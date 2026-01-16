ccb743b3dfc80bee365875dea141ae00c92a7269
Evina <evinashingavi@gmail.com>
2026-01-15 22:53:22 -0500

    Push all recent changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar sidebar-navbar">
       <ul className="navbar-links">
         <li>
           <Link to="/app">
